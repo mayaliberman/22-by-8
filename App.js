937eeb4fb931afeaa7d9da378f499a35a9228fd5
Maya Liberman <mayaliberman1@gmail.com>
2021-04-11 19:00:09 +0300

    adding calender

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>22:8 Cycles Home Page</Text>
+      <Calendar
+        current={'2021-04-11'}
+        style={{
+          borderWidth: 1,
+          borderColor: 'gray',
+          height: 350,
+        }}
+      />
+      <Button style={styles.button} title="New Productive Day" />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +27,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: '#555',
+  },
+  calender: {
+    margin: 20,
+    borderColor: 'gray',
+    borderWidth: 1,
+    height: 350,
   },
 });
